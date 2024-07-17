f7c775875f6269b459afd8afe46d1bd1c5fe84fb
nirojneupane5 <nirojneupane5@gmail.com>
2024-07-17 15:12:52 +0545

    Shadcn UI added

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import "./App.css";
+import { Button } from "./components/ui/button";
 
 function App() {
   return (
     <>
       <div className="text-red-400">Hello Zustand</div>
+      <Button>Hello</Button>
     </>
   );
 }
